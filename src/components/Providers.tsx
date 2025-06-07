0411400e74ad4ee0a253cc052b3508a1d4f1b96d
DuyNQ20 <quangduy.family@gmail.com>
2025-06-07 17:45:54 +0700

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -28,7 +28,7 @@ const Providers = async (props: Props) => {
   const systemMode = await getSystemMode()
 
   return (
-    <NextAuthProvider basePath={process.env.NEXTAUTH_BASEPATH}>
+    <NextAuthProvider>
       <VerticalNavProvider>
         <SettingsProvider settingsCookie={settingsCookie} mode={mode}>
           <ThemeProvider direction={direction} systemMode={systemMode}>
