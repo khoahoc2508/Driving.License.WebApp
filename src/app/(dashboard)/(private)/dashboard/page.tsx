016865e2e77dbfcf86d94b4a081413a849fe9440
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2025-06-24 22:05:31 +0700

    chore(dashboard): lint fix

--- src/app/(dashboard)/(private)/dashboard/page.tsx
+++ src/app/(dashboard)/(private)/dashboard/page.tsx
@@ -1,30 +1,29 @@
 // MUI Imports
-import type { ReactElement } from 'react'
 
 import dynamic from 'next/dynamic'
 
-import Grid from '@mui/material/Grid2'
 
 // Next Imports
 
 // Component Imports
-import Dashboard from '@/views/dashboard'
 
 
 
 // Data Imports
 
 const OverViewTab = dynamic(() => import('@views/dashboard/overview'))
-const RevenueTab = dynamic(() => import('@views/dashboard/revenue'))
+
+// const RevenueTab = dynamic(() => import('@views/dashboard/revenue'))
 
 // Vars
-const tabContentList = (): { [key: string]: ReactElement } => ({
-  overview: <OverViewTab />,
-  revenue: <RevenueTab />
-})
+// const tabContentList = (): { [key: string]: ReactElement } => ({
+//   overview: <OverViewTab />,
+//   revenue: <RevenueTab />
+// })
 
 const dashboard = async () => {
   return (
+
     // <Grid container spacing={6}>
     //   <Grid size={{ xs: 12 }}>
     //     {/* <ExamSchedulesTable /> */}
