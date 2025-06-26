cd4f096f6f15400e1bb5e29d5ae18ef4ffe9d1b4
Ngô Kiên <101339998+NgoKien15802@users.noreply.github.com>
2025-06-26 22:50:55 +0700

    fix: lint

--- src/app/front-pages/landing-page/page.tsx
+++ src/app/front-pages/landing-page/page.tsx
@@ -2,13 +2,9 @@
 import LandingPageWrapper from '@views/front-pages/landing-page'
 
 // Server Action Imports
-import { getServerMode } from '@core/utils/serverHelpers'
-
 const LandingPage = async () => {
-  // Vars
-  const mode = await getServerMode()
 
-  return <LandingPageWrapper mode={mode} />
+  return <LandingPageWrapper />
 }
 
 export default LandingPage
